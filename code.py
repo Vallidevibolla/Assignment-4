ece0ad4bdb3f7b53cc5e21986d685d6a50f7c397
Vallidevibolla <86243949+Vallidevibolla@users.noreply.github.com>
2021-08-27 21:56:33 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.axis([4,8,4,8])
+
+plt.axis('on')
 
 
 #if using termux
@@ -46,14 +49,22 @@ x_AB = line_gen(A,B)
 
 #Plotting all lines
 plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(0,2,'o')
+plt.plot(4,0,'o')
+plt.plot(2,0,'o')
+plt.plot(1.4,5.65,'o')
+plt.plot(1,1,'o')
 
-plt.plot(A[0], A[1], 'o')
-plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A')
-plt.plot(B[0], B[1], 'o')
-plt.text(B[0] * (1 - 0.2), B[1] * (1) , 'B')
 
-plt.xlabel('$x$')
-plt.ylabel('$y$')
+plt.xlabel('$x-axis$')
+plt.ylabel('$y-axis$')
+
+
+plt.text(0,2, 'A(0,2)')
+plt.text(4,0, 'B(4,0)')
+plt.text(2,0, 'C(2,0)')
+plt.text(1.4,5.65, 'D(1.4,5.65)')
+plt.text(1,1, 'E(1,1)')
 plt.legend(loc='best')
 plt.grid() # minor
 plt.axis('equal')
